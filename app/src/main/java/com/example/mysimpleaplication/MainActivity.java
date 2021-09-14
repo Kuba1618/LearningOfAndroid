6ea2c0606545e9360b214efd206eef6ab991b837
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-09-14 15:46:04 +0200

    Make button enabled

--- app/src/main/java/com/example/mysimpleaplication/MainActivity.java
+++ app/src/main/java/com/example/mysimpleaplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.mysimpleaplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onHelloBtnClicked(View view) {
+        TextView txtWelcome = findViewById(R.id.txtWelcome);
+        txtWelcome.setText("Hi there");
 
     }
 }
