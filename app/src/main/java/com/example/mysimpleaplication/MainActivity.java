5a5898500924ff35908172fd695827d4e858bf6a
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-09-15 21:30:53 +0200

    Use OnClickListener interface and print some text in terminal

--- app/src/main/java/com/example/mysimpleaplication/MainActivity.java
+++ app/src/main/java/com/example/mysimpleaplication/MainActivity.java
@@ -4,20 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.btnHello:
+                System.out.println("Hello");
+                break;
+            default:
+                break;
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(this);
     }
 
-    public void onHelloBtnClicked(View view) {
-        TextView txtWelcome = findViewById(R.id.txtWelcome);
-        txtWelcome.setText("Hi there");
 
-    }
+
+//    public void onHelloBtnClicked(View view) {
+//        TextView txtWelcome = findViewById(R.id.txtWelcome);
+//        txtWelcome.setText("Hi there");
+//
+//    }
 }
