622199e8a073f6d03bf953eec56c46c6cbd6dedd
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-10-01 22:59:32 +0200

    Create a new string.xml file to add Polish language

--- app/src/main/java/com/example/mysimpleaplication/MainActivity.java
+++ app/src/main/java/com/example/mysimpleaplication/MainActivity.java
@@ -21,11 +21,15 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
 
+    private TextView txtHello;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtHello = findViewById(R.id.txtHello);
+        txtHello.setText(getString(R.string.hello));
     }
 
 }
