9cb48797ab6e6336dea24c157fdae7429a718331
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-10-01 22:38:43 +0200

    Clear things

--- app/src/main/java/com/example/mysimpleaplication/MainActivity.java
+++ app/src/main/java/com/example/mysimpleaplication/MainActivity.java
@@ -20,69 +20,12 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView citiesListJava;
-    private Spinner studentsSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        citiesListJava = findViewById(R.id.citiesListXML);
-        studentsSpinner = findViewById(R.id.studentsSpinner);
-
-//        ArrayList<String> students = new ArrayList<>();
-//        students.add("Kamil");
-//        students.add("Lolek");
-//        students.add("Bolek");
-//        students.add("Krzysztof");
-//        students.add("Teresa");
-//        students.add("Bocian");
-//
-//        ArrayAdapter<String> studentsAdapter = new ArrayAdapter<>(
-//                this,
-//                android.R.layout.simple_spinner_dropdown_item,
-//                students
-//        );
-//
-//        studentsSpinner.setAdapter(studentsAdapter);
-
-        studentsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this, studentsSpinner.getSelectedItem().toString() + " Seleceted", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-
-            }
-        });
-
-
-        ArrayList<String> citiestArrayList = new ArrayList<>();
-        citiestArrayList.add("Kraków");
-        citiestArrayList.add("Katowice");
-        citiestArrayList.add("Kielce");
-        citiestArrayList.add("Poznań");
-        citiestArrayList.add("Śródmorze");
-        citiestArrayList.add("Bieliny");
-
-
-        ArrayAdapter<String> citiesAdapter = new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                citiestArrayList
-        );
-
-        citiesListJava.setAdapter(citiesAdapter);
-
-        citiesListJava.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(MainActivity.this,citiestArrayList.get(i) + " Selected", Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 
 }
