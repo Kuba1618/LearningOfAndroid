5e503531c116fdc12d1a8c1c01d951ab10762876
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-10-03 10:50:11 +0200

    Create the menu items and enable it by send you toast messsage

--- app/src/main/java/com/example/mysimpleaplication/MainActivity.java
+++ app/src/main/java/com/example/mysimpleaplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.mysimpleaplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -23,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtHello;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,4 +38,28 @@ public class MainActivity extends AppCompatActivity {
         txtHello.setText(getString(R.string.hello));
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.settings_menu: {
+                Toast.makeText(this," Settings Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.alarm_menu: {
+                Toast.makeText(this, " Alarm Selected", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+
+    }
 }
